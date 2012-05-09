3cd5762a050f38da9c218afcd5e0c42689a988b4
Łukasz Gwiazda <lgwiazda@gmail.com>
2012-05-09 12:15:03 +0300

    Update composite_newton_cotes.m

--- composite_newton_cotes.m
+++ composite_newton_cotes.m
@@ -1,6 +1,6 @@
 function [ integral ] = composite_newton_cotes( f, M, a, b )
 
-x = linspace(a, b, M);
+x = linspace(a, b, M+1);
 integral = (b-a)/M/2 * (f(a) + 2*sum(arrayfun(f,x(2:M))) + f(b));
 
 end
